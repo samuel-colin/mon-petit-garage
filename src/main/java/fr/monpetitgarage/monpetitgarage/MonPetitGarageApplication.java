4dfadd5583c8efcd1af174f294fe81cb4de76e17
samuel.colin <samuel.colin@adventiel.fr>
2022-04-11 22:52:54 +0200

    Initialisation du projet

---
+++ src/main/java/fr/monpetitgarage/monpetitgarage/MonPetitGarageApplication.java
@@ -0,0 +1,13 @@
+package fr.monpetitgarage.monpetitgarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonPetitGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonPetitGarageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/monpetitgarage/monpetitgarage/MonPetitGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.monpetitgarage.monpetitgarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonPetitGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
